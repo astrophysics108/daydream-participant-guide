5116702c9c9ca45342e58327f4ecbef35426c878
Astrophysics108 <dsingh@perse.co.uk>
2025-09-20 18:47:18 +0100

    deploy to pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://astrophysics108.github.io',
+    base: '/daydream-participant-guide',
+});
